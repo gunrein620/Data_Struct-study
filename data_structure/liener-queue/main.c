e8c736b780ae92c1bd9f6ba8018aecb817a4e1e8
gunrein <gunrein620@gmail.com>
2025-04-19 23:11:29 +0900

    큐 프로그램에 종료 메시지 추가 및 출력 형식 수정

--- data_structure/liener-queue/main.c
+++ data_structure/liener-queue/main.c
@@ -16,20 +16,24 @@ init(&qu);
 printf("아무키나 누르면 종료\n");
   while (1)
   {
-    printf("\n");
     printf("인큐: + | 디큐: -   입력하세요: ");
     scanf("%c", &buf);
     getchar();
     if (buf == '+') {
       printf("인큐할 숫자: ");
       scanf("%d",&input);
+      getchar();
       enqueue(&qu, input);
     }
     else if (buf == '-')
     {
       printf("디큐된 숫자:");
       elem_t de = dequeue(&qu);
-      printf("%d",de);
+      printf("%d\n",de);
+    }
+    else{ 
+      printf("프로그램을 종료합니다.");
+      return 0;
     }
   }
   printf("\n");
